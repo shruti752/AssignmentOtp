bc30cfbcdcc1252e8de6b64fe628924427a0dbf2
shruti752 <shrutishikha18@gmail.com>
2020-11-23 15:27:06 -0500

    Initial commit

---
+++ assignment/src/main/java/com/assignment/assignment/CountCharacter.java
@@ -0,0 +1,10 @@
+package com.assignment.assignment;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class CountCharacter {
+
+}
---
+++ assignment/src/main/java/com/assignment/assignment/OTP.java
@@ -0,0 +1,63 @@
+package com.assignment.assignment;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class OTP {
+
+    //for OTP
+    public static char[] otp(int len)
+    {
+        String numbers = "0123456789";
+        Random random = new Random();
+        char[] otp = new char[len];
+
+        for(int i = 0;i<len;i++)
+        {
+            otp[i] = numbers.charAt(random.nextInt(numbers.length()));
+        }
+        return otp;
+    }
+
+    @RequestMapping( value ="/otp",method = RequestMethod.GET)
+    public static char[]  Callotp(){
+        int length = 4;
+        return otp(length);
+    }
+
+
+
+    //for countcharacter of string
+
+    public static String countEachCharacter(String str){
+        int count[] = new int[256];
+        int len  = str.length();
+
+        for(int i = 0;i<len;i++)
+            count[str.charAt(i)]++;
+
+        char array[] = new char[str.length()];
+        for (int i= 0;i<len;i++){
+            array[i] = str.charAt(i);
+            int num = 0;
+            for (int j = 0;j<=i;j++){
+                if (str.charAt(i) == array[j])
+                    num ++;
+            }
+            if (num == 1)
+                return ("Occurence of char "+ str.charAt(i)+ "in the string is:"+count[str.charAt(i)]);
+        }
+        return "";
+    }
+    @RequestMapping(value = "/countcharacter",method = RequestMethod.GET)
+    public static String callMethod(){
+        String string = "shrutishikha";
+        return countEachCharacter(string);
+    }
+
+
+}
